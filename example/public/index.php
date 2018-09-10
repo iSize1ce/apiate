652d8c7deff98e63783cdfb691464e4e241d81ca
Pavel Leschev <p.leschev@ibc.ru>
2018-09-10 15:35:18 +0500

    Refactoring

--- example/public/index.php
+++ example/public/index.php
@@ -15,7 +15,7 @@ $request = Request::createFromGlobals();
 try {
     $response = $apiate->getResponseByRequest($request);
 } catch (Exception $exception) {
-    $apiate->sendResponse(new JsonResponse(['error' => $exception->getMessage()], 500));
+    $response = new JsonResponse(['error' => $exception->getMessage()], 500);
 }
 
 if ($response !== null) {
