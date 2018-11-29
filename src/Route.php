0244fe619a85267477a04eebac70b33160cc1ee3
Pavel Leschev <isize1ce@gmail.com>
2018-11-30 00:04:31 +0500

    Add route properties

--- src/Route.php
+++ src/Route.php
@@ -4,7 +4,25 @@ namespace Apiate;
 
 class Route
 {
+    /**
+     * @var string
+     */
+    private $method;
+
+    /**
+     * @var string
+     */
+    private $path;
+
+    /**
+     * @var HandlerInterface
+     */
+    private $handler;
+
     public function __construct(string $method, string $path, HandlerInterface $handler)
     {
+        $this->method = $method;
+        $this->path = $path;
+        $this->handler = $handler;
     }
 }
