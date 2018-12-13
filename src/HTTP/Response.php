5e9cd054fbba899ed79b7200dac04fac4679e5b3
Pavel Leschev <isize1ce@gmail.com>
2018-12-13 23:47:39 +0500

    Send cookies

--- src/HTTP/Response.php
+++ src/HTTP/Response.php
@@ -14,13 +14,27 @@ class Response
      */
     private $headers;
 
+    /**
+     * @var Parameters
+     */
+    private $cookies;
+
     public function __construct()
     {
         $this->headers = new Parameters();
+        $this->cookies = new Parameters();
     }
 
     public function send(): void
     {
+        $cookies = [];
+        foreach ($this->getCookies() as $name => $value) {
+            $cookies[] = "$name=$value";
+        }
+        if (count($cookies) > 0) {
+            header('Cookie: ' . implode(';', $cookies));
+        }
+
         foreach ($this->getHeaders() as $name => $value) {
             header("$name:$value");
         }
@@ -41,5 +55,15 @@ class Response
     public function getHeaders(): Parameters
     {
         return $this->headers;
+    }
+
+    public function setCookies(Parameters $cookies): void
+    {
+        $this->cookies = $cookies;
+    }
+
+    public function getCookies(): Parameters
+    {
+        return $this->cookies;
     }
 }
