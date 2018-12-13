ccbc4fb6dd58137789a52a524eab06891dd503b8
Pavel Leschev <isize1ce@gmail.com>
2018-12-13 23:58:53 +0500

    Test

--- example/index.php
+++ example/index.php
@@ -6,12 +6,18 @@ include_once __DIR__ . '/../vendor/autoload.php';
 
 $app = new Apiate();
 
+set_exception_handler(function (Throwable $throwable) use ($app) {
+    if ($throwable instanceof \Apiate\RouteNotFoundException) {
+        $app->sendResponse(new \Apiate\HTTP\Response());
+    }
+});
+
 $routes = $app->getRoutes();
 
 $routes->handle('/',
     [Request::METHOD_GET, Request::METHOD_POST, Request::METHOD_DELETE, Request::METHOD_PUT],
     new ClosureHandler(function (Request $request) {
-        return new JsonResponse([
+        $response = new JsonResponse([
             'host' => $request->getHeaders()->offsetGet('HOST'),
             'path' => $request->getUriPath(),
             'uriParameters' => $request->getUriParameters()->getArrayCopy(),
@@ -19,6 +25,10 @@ $routes->handle('/',
             'cookies' => $request->getCookies()->getArrayCopy(),
             'files' => $request->getFiles()->getArrayCopy()
         ]);
+
+        $response->getCookies()->offsetSet('test', rand(0, 1000));
+
+        return $response;
     })
 );
 
