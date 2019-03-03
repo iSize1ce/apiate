9ef1a71a97d91ba06e3d6520c9cd9f15596487d7
Pavel Leschev <isize1ce@gmail.com>
2019-03-03 21:29:45 +0500

    Fix

--- example/index.php
+++ example/index.php
@@ -6,6 +6,7 @@ use Apiate\Route\RouteProvider;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\ParameterBag;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 include_once __DIR__ . '/../vendor/autoload.php';
 
