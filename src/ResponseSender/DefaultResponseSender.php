476f2bbbdba99ee1f9d18486a3aabd785cc1d004
Pavel Leschev <isize1ce@gmail.com>
2019-03-10 16:44:08 +0500

    Add request sender psr7

--- src/ResponseSender/DefaultResponseSender.php
+++ src/ResponseSender/DefaultResponseSender.php
@@ -3,11 +3,29 @@
 namespace Apiate\ResponseSender;
 
 use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
 
 class DefaultResponseSender implements ResponseSenderInterface
 {
     public function send(ResponseInterface $response): void
     {
-        $response->send();
+        if ($response instanceof ServerRequestInterface) {
+            // Cookies
+            foreach ($response->getCookieParams() as $name => $value) {
+                setcookie($name, $value);
+            }
+        }
+
+        // Headers
+        foreach ($response->getHeaders() as $name => $values) {
+            foreach ($values as $value) {
+                header("$name: $value");
+            }
+        }
+
+        // Http status
+        header("HTTP/{$response->getProtocolVersion()} {$response->getStatusCode()} {$response->getReasonPhrase()}");
+
+        echo $response->getBody()->getContents();
     }
 }
